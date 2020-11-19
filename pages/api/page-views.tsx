66ce3c258c353894698abc71c837d5796861fec2
Gui-dev <adrianon10@gmail.com>
2020-11-19 03:04:17 -0300

    first commit

---
+++ pages/api/page-views.tsx
@@ -0,0 +1,33 @@
+import { connectToDatabase } from '@/config/mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+
+  const slug = request.query.id
+
+  if (!slug) {
+    return response.status(404).json('Página não encontrada')
+  }
+
+  const { client, db } = await connectToDatabase()
+
+  if (client.isConnected()) {
+    const pageViewBySlug = await db.collection('pageviews').findOne({ slug })
+    let total = 0
+  
+    if (pageViewBySlug) {
+      total = pageViewBySlug.total + 1
+      await db.collection('pageviews').updateOne({ slug }, { $set: { total } })
+    } else {
+      total = 1
+      await db.collection('pageviews').insertOne({ slug, total })
+    }
+
+    return response.status(200).json({ total })
+  }
+
+  return response.status(500).json({
+    error: 'Client DB is not connected'
+  })
+
+}
